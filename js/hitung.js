b01e3268cc190f3bf0a5e64a10f741ff6b03646d
Fajar Septian <fajarajay10@gmail.com>
2019-05-03 23:16:45 +0700

    update calculation

--- js/hitung.js
+++ js/hitung.js
@@ -1,15 +1,37 @@
 function clearNumber() {
-  console.log("clearNumber")
-  document.getElementById('input-number').innerHTML = "";
-  document.getElementById('result-calc').innerHTML = "";
+  document.getElementById('input-number').innerHTML = "0";
+  document.getElementById('result-calc').innerHTML = "0";
 }
 
 function calculateResult() {
-  var inputan = document.getElementById('input-number').innerHTML;
-  console.log("inputan calculateResult " + document.getElementById('input-number').innerHTML);
-  document.getElementById('result-calc').innerHTML = eval(inputan.replace( % , "/100"));
+  document.getElementById('result-calc').innerHTML = eval(document.getElementById('input-number').innerHTML);
+}
+
+function square(inputan) {
+  document.getElementById('result-calc').innerHTML = eval(inputan) * eval(inputan)
+}
+
+function checkNum(str) {
+  for (var i = 0; i < str.length; i++) {
+    var ch = str.charAt(i);
+    if (ch < "0" || ch > "9") {
+      if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch != "." &&
+        ch != "(" && ch != ")" && ch != "%") {
+        alert("invalid entry!")
+        return false
+      }
+    }
+  }
+  return true
 }
 
 function deleteChar(inputan) {
   document.getElementById('input-number').innerHTML = inputan.substring(0, inputan.length - 1);
+}
+
+function addChar(character) {
+  if (document.getElementById('input-number').innerHTML == "0")
+    document.getElementById('input-number').innerHTML = character
+  else
+    document.getElementById('input-number').innerHTML += character
 }
