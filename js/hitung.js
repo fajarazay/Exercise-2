adc3964ab6d5bc2516a507411737a5b90a08dec9
Fajar Septian <fajarajay10@gmail.com>
2019-05-04 14:31:25 +0700

    optimize function hitung

--- js/hitung.js
+++ js/hitung.js
@@ -1,37 +1,52 @@
+function getId(selector) {
+  return document.getElementById(selector);
+}
+
 function clearNumber() {
-  document.getElementById('input-number').innerHTML = "0";
-  document.getElementById('result-calc').innerHTML = "0";
+  getId("input-number").innerHTML = "0";
+  getId("result-calc").innerHTML = "0";
 }
 
 function calculateResult() {
-  document.getElementById('result-calc').innerHTML = eval(document.getElementById('input-number').innerHTML);
+  getId("result-calc").innerHTML = eval(getId("input-number").innerHTML);
 }
 
 function square(inputan) {
-  document.getElementById('result-calc').innerHTML = eval(inputan) * eval(inputan)
+  getId("result-calc").innerHTML = eval(inputan) * eval(inputan);
 }
 
 function checkNum(str) {
   for (var i = 0; i < str.length; i++) {
     var ch = str.charAt(i);
     if (ch < "0" || ch > "9") {
-      if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch != "." &&
-        ch != "(" && ch != ")" && ch != "%") {
-        alert("invalid entry!")
-        return false
+      if (
+        ch != "/" &&
+        ch != "*" &&
+        ch != "+" &&
+        ch != "-" &&
+        ch != "." &&
+        ch != "(" &&
+        ch != ")" &&
+        ch != "%"
+      ) {
+        alert("invalid entry!");
+        return false;
       }
     }
   }
-  return true
+  return true;
 }
 
 function deleteChar(inputan) {
-  document.getElementById('input-number').innerHTML = inputan.substring(0, inputan.length - 1);
+  if (inputan.length == 1) {
+    getId("input-number").innerHTML = "0";
+  } else {
+    getId("input-number").innerHTML = inputan.substring(0, inputan.length - 1);
+  }
 }
 
 function addChar(character) {
-  if (document.getElementById('input-number').innerHTML == "0")
-    document.getElementById('input-number').innerHTML = character
-  else
-    document.getElementById('input-number').innerHTML += character
+  if (getId("input-number").innerHTML == "0")
+    getId("input-number").innerHTML = character;
+  else getId("input-number").innerHTML += character;
 }
